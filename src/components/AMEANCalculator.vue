f32f838f82c9891d320e7e4a4e7362622773e2d3
eminkartci <emin.kartci@ozu.edu.tr>
2022-03-22 11:14:59 +0300

    Vue Project is initialized - AMEAN Calculator Component

---
+++ src/components/AMEANCalculator.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    Hello World
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: 'Calculator',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
